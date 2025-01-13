676317793aaae6f97f70f72c15e27ea1e62624ae
urosveljkovic <uros.veljkovic1996@gmail.com>
2025-01-13 18:43:41 +0100

    First commit

---
+++ src/main/kotlin/com/example/quizler/quizler/spring/QuizlerSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.quizler.quizler.spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizlerSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuizlerSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/quizler/quizler/spring/QuizlerSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.quizler.quizler.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuizlerSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
